bd735c6126acfd5d239227cb2fb90adbba2955bc
mesut <mkoc1367@gmail.com>
2023-05-27 00:19:43 +0200

    m

---
+++ tests/test_hesapmakinesi.py
@@ -0,0 +1,17 @@
+def toplama(a, b):
+    return a + b
+
+def cikarma(a, b):
+    return a - b
+
+def bolme(a, b):
+    return a/b
+
+def carpma(a, b):
+    return a * b
+
+def test_toplamTesti():
+    assert toplama(2,2)==4
+
+def test_cikar():
+    assert cikarma(4,1)==4
