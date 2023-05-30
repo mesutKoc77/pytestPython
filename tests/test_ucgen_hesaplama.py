a004e87aae2aec650c2d2985f24aaa5d4d9514a4
mesutKoc77 <mkoc1367@gmail.com>
2023-05-30 09:49:09 +0200

    ucgen

---
+++ tests/test_ucgen_hesaplama.py
@@ -0,0 +1,10 @@
+def cevre_hesaplama(a,b,c):
+    return a+b+c
+def ucgenalanhesaplama(taban, yukseklik):
+    return taban*yukseklik/2
+
+def test_ucgenCevresi():
+    assert 10==cevre_hesaplama(7,3,0)
+
+def test_ucgenAlanHesapla():
+    assert 5==ucgenalanhesaplama(5,2)
