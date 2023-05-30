0ef07f81b4584c2ab9095b5456002102c8626147
mesutKoc77 <mkoc1367@gmail.com>
2023-05-30 11:01:38 +0200

    ucgen

--- tests/test_ucgen_hesaplama.py
+++ tests/test_ucgen_hesaplama.py
@@ -4,7 +4,9 @@ def ucgenalanhesaplama(taban, yukseklik):
     return taban*yukseklik/2
 
 def test_ucgenCevresi():
+    print("ucgencevresutesti")
     assert 10==cevre_hesaplama(7,3,0)
 
 def test_ucgenAlanHesapla():
-    assert 5==ucgenalanhesaplama(5,2)
+    print("yazdi mi")
+    assert 7==ucgenalanhesaplama(5,2)
