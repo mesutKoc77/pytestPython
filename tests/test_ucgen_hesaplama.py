c15fe2133507d7bce6f9bdaeabf97f761bbacc16
mesutKoc77 <mkoc1367@gmail.com>
2023-05-31 13:02:03 +0200

    yenilik

--- tests/test_ucgen_hesaplama.py
+++ tests/test_ucgen_hesaplama.py
@@ -7,12 +7,20 @@ def ucgenalanhesaplama(taban, yukseklik):
     return taban*yukseklik/2
 
 
+@pytest.fixture()
+def ucgen():
+    print("ucgen olustu ve fixture kullanildi ve ilgili url e gidildi")
+    yield
+    print("ucgeni sildim ve ana sayfaya dondum")
 
-@pytest.mark.skip(reason="henuz tamamlanmadi")
-def test_ucgenCevresi():
-    print("ucgencevresutesti")
+    #fixture ile herbir testin baslamadan once yapacaklari belirleniyor ve onun icindeki
+    #method calisiyor ve en son yield ile sonlandirip istersem basa da donus saglayabilirim
+    # #
+
+def test_ucgenCevresi(ucgen):
+    print("ucgencevrehesaplama")
     assert 10==cevre_hesaplama(7,3,0)
-@pytest.mark.skipif (windowsmu=="evet", reason="windows olmamali")
-def test_ucgenAlanHesapla():
-    print("yazdi mi")
+
+def test_ucgenAlanHesapla(ucgen):
+    print("ucgen alan hesaplama")
     assert 5==ucgenalanhesaplama(5,2)
