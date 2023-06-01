4856a6046f43899a272ed1e2dd7df7a684705c78
mesutKoc77 <mkoc1367@gmail.com>
2023-06-01 16:53:30 +0200

    yenilik

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -3,8 +3,6 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-
-
 class TestHomepage:
     def test_top_menu_items(self):
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
@@ -21,4 +19,32 @@ class TestHomepage:
         for i in range(len(expected_menu)):
             assert expected_menu[i]==actual_menu_items[i]
         driver.quit()
+
+    def test_urun_ismine_tiklar_ve_urun_sayfasi_acilir(self):
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        driver.maximize_window()
+        driver.get("https://demowebshop.tricentis.com/")
+        ilkurunlinki=driver.find_element(By.CSS_SELECTOR,"div.product-item h2 a")
+        urun_ismi=ilkurunlinki.text
+        urunfiyati=driver.find_element(By.CSS_SELECTOR,"span.price.actual-price").text
+        ilkurunlinki.click()
+        urunismidetaysayfasi=driver.find_element(By.CSS_SELECTOR,"div.product-name h1").text.strip()
+        urunfiyatidetaysayfasi=driver.find_element(By.CSS_SELECTOR,"div.product-price span").text.strip()
+
+        assert urun_ismi==urunismidetaysayfasi
+        assert urunfiyati==urunfiyatidetaysayfasi
+
+        driver.quit()
+
+
+
+
+
+
+
+
+
+
+
+
 
--- tests/test_kare_hesaplama.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.mark.smoke
-@pytest.mark.armut
-def test_kare_alan_hesaplama():
-    assert 4==2*2
